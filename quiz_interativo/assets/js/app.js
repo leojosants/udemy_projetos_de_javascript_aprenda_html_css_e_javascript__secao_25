3000b17ef44361ec227044986a4a5c6b4f8bf332
Leonardo Santos <leonardojosantos@gmail.com>
2023-09-20 17:43:22 -0300

    class 131 - finishing the quiz logic

--- quiz_interativo/assets/js/app.js
+++ quiz_interativo/assets/js/app.js
@@ -6,8 +6,8 @@ import { questions } from "./questions_db.js";
 */
 const question_element = document.querySelector('[data_question]');
 const choice_elements = document.querySelectorAll('[data_choice]');
-const score_elements = document.querySelector('[data_score]');
-const wrong_elements = document.querySelector('[data_wrong]');
+const score_element = document.querySelector('[data_score]');
+const wrong_element = document.querySelector('[data_wrong]');
 const next_button = document.querySelector('[data_next]');
 
 /*
@@ -53,8 +53,58 @@ const shuffleArray = (array) => {
 };
 
 // 
+const checkAnswer = (event) => {
+    if (answer_chosen) return;
+    answer_chosen = true;
+
+    if (event.target.innerText === questions[current_question].answer) {
+        score++;
+        score_element.innerText = `Pontuação: ${score}`;
+        alert('Correto!');
+    }
+    else {
+        wrong++;
+        wrong_element.innerText = `Erros: ${wrong}`;
+        alert(`Errado! A resposta correta é: ${questions[current_question].answer}`);
+    };
+};
+
+// 
+const nextQuestion = () => {
+    if (!answer_chosen) {
+        alert('Por favor, responda a pergunta!');
+        return;
+    };
+
+    current_question++;
+
+    if (current_question < questions.length) {
+        loadQuestion();
+    }
+    else {
+        alert(`Fim de jogo! Você acertou ${score} de ${questions.length} perguntas.`);
+        restartQuiz();
+    };
+};
+
+const restartQuiz = () => {
+    current_question = 0;
+    score = 0;
+    wrong = 0;
+    score_element.innerText = `Pontuação: 0`;
+    wrong_element.innerText = `Erros: 0`;
+    loadQuestion();
+};
+
+// 
 loadQuestion();
 
 /*
     events
 */
+choice_elements.forEach((button) => {
+    button.addEventListener('click', checkAnswer);
+});
+
+// 
+next_button.addEventListener('click', nextQuestion);
